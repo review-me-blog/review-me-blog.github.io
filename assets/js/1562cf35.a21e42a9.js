f556a868b7dc7cfc444010278685f05e04b717dc
BadaHertz52 <BadaHertz52@users.noreply.github.com>
2024-08-26 14:28:41 +0000

    deploy: 4aa981e98cd5e41557a7ce5201a624af4a3f68bb

---
+++ assets/js/1562cf35.a21e42a9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[2218],{6828:l=>{l.exports=JSON.parse('{"tag":{"label":"blog","permalink":"/blog/tags/blog","allTagsPath":"/blog/tags","count":6,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":6,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
